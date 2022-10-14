255bc8ac2c0d38fd1d1c8900524f497415e9abf3
Yaroslav Shichkin <yaroslavshichkin@mail.ru>
2022-10-15 02:11:26 +0300

    task_2

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {'Content-type': 'application/json',
+                'Authorization': f'OAuth {self.token}'}
+
+    def upload(self, file_path: str):
+        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {'path': file_path, 'overwrite' : 'true'}
+        response = requests.get(url, headers = headers, params = params)
+        response = response.json()
+        href = response.get('href', '')
+        result = requests.put(href, data=open('text.txt', 'rb'))
+
+
+
+if __name__ == '__main__':
+    path_to_file = 'text.txt'
+    # token = input('Введите токен:')
+    token = 'y0_AgAAAAA30xPxAADLWwAAAADREj-b8Y0x5SSOR6uqm2_hdruIS2qiEMo'
+    uploader = YaUploader(token)
+    result = uploader.upload(path_to_file)
